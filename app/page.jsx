bc4bed2df9202a92859d2230fbbaeff98da830f1
Rodevp <rodriguezrodrigor12@gmail.com>
2023-05-06 19:04:56 -0500

    calculator styles ready

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import ColombiaFlag from "../assets/colombia.png"
+import LogoChange from "../assets/exchange.svg"
 
 export const metadata = {
   title: "Dolar hoy"
@@ -55,12 +56,34 @@ export default function Home() {
           "
         >
           <div
-            className="w-full h-[45%] rounded-lg p-2 bg-slate-400 bg-clip-border shadow-md md:h-[65%]"
+            className="w-full h-[45%] rounded-lg p-2 bg-white bg-clip-border shadow-md md:h-[65%] md:flex md:flex-col md:justify-center"
           >
-
+            <div>
+              <input 
+                type="text"
+                className="w-full p-4 text-slate-800 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 "  
+              />
+            </div>
+            <div
+              className="w-full flex justify-center mb-2 mt-2"
+            >
+              <Image
+                src={LogoChange}
+                alt="change logo"
+                width={35}
+                height={35}
+                className="cursor-pointer"
+              />
+            </div>
+            <div>
+            <input 
+              type="text"
+              className="w-full p-4 text-slate-800 border border-gray-300 rounded-lg bg-gray-50 sm:text-md "   
+            />
+            </div>
           </div>
           <div
-            className="w-full h-[45%] rounded-lg p-2 bg-slate-400 bg-clip-border shadow-md md:h-[65%]"
+            className="w-full h-[45%] rounded-lg p-2 bg-white bg-clip-border shadow-md md:h-[65%]"
           >
 
           </div>
