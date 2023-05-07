e1d91558ed62036421b899c680d781562e5aed1f
Rodevp <rodriguezrodrigor12@gmail.com>
2023-05-06 20:10:51 -0500

    write value dollar in txt

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server"
 import chieero from "cheerio"
 import dotenv from "dotenv" 
+import fs from "fs"
 
 dotenv.config()
 
@@ -12,6 +13,10 @@ export async function GET() {
     const scrap = chieero.load( page )
 
     const valueDollar = scrap('span.exchange-rate')
+
+    const writeTxt = fs.createWriteStream("./app/api/dolarToday.txt", { flags: 'a' })
+    writeTxt.write(`${valueDollar.text()}\n`)
+    writeTxt.end()
 
     return NextResponse.json({
         message: "value dollar",
