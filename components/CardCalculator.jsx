20880803e83e583ccaabd71e6a21c9f4f0d1b734
Rodevp <rodriguezrodrigor12@gmail.com>
2023-05-08 14:05:15 -0500

    dollar card ready

--- components/CardCalculator.jsx
+++ components/CardCalculator.jsx
@@ -35,22 +35,26 @@ function CardCalculator() {
     
     const resultConvert = Number(resultOperation).toLocaleString("en", {currency: "COP"}) 
 
-    console.log('intl', resultConvert)
-
     return (
         <div
             className="w-full h-[45%] rounded-lg p-2 bg-white bg-clip-border shadow-md md:h-[65%] md:flex md:flex-col md:justify-center"
         >
+            <p
+                className="text-slate-800 border mb-2 px-2 text-1xl border-none"
+            >
+                El valor se calcula colocando de dolares a pesos, esté automaticamente irá procesando
+                a medida que su valor vaya cambiando.💰
+            </p>
             <div>
                 <input
                     onChange={handleCurrentMoney}
                     placeholder="Escribe tu valor en dolares: 2000"
                     type="text"
-                    className="w-full p-4 text-slate-800 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 "
+                    className="w-full p-4 mb-2 text-slate-800 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 "
                 />
             </div>
             <h3
-                className="text-slate-800 text-2xl mb-2"
+                className="text-slate-800 text-2xl mb-2 px-1"
             >
                Valor: { resultConvert } COP
             </h3>
